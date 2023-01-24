a7970b3e98df5a99ad696e37eb774bf4ce8c3c18
kailixie <114327449+kailixie@users.noreply.github.com>
2023-01-24 12:27:29 -0500

    Complete Odin JavaScript exercise 04
    Create function to remove multiple items from an array.
    Remove .skips to make sure function passes all tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+//    index = arr.indexOf(toRemove);
+//    arr.splice(index, 1);
+//    return arr
 
-};
+    const arrRemove = Array.from(toRemove);
+    let newArray = []
+
+    for (let i = 0; i < arr.length; i++) {        
+//        index = arr.indexOf(i);        
+        if (arrRemove.includes(arr[i]) == false) {
+            newArray.push(arr[i]);
+        }else {
+            continue;         
+        }  
+    }   return newArray;
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
