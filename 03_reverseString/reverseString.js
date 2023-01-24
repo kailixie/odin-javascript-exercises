4b655d4b8293ac357f7ae28d61c3f88924373961
kailixie <114327449+kailixie@users.noreply.github.com>
2023-01-23 21:33:49 -0500

    Complete Odin JavaScript exercise 03
    Add function to reverse string.
    Remove skips from test file to make sure function passes all tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    arr = text.split("");
+    reverseArr = arr.reverse();
+    newString = reverseArr.join("");
+    return newString
 };
 
 // Do not edit below this line
