f3736e7370fbe6d72d278aa36b5c96ecdce933ea
kailixie <114327449+kailixie@users.noreply.github.com>
2023-01-24 18:10:31 -0500

    Complete Odin JavaScript exercise 05
    Create function to take two integers and return the sum of every number between (and including) them.
    
    Remove .skips to test functions passes all tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,44 @@
-const sumAll = function() {
+const sumAll = function(integerOne, integerTwo) {
+    function checkType(a, b) {
+        if (typeof a == "number" && typeof b == "number"){
+            return "Y";
+        } else {
+            return "N";
+        }
+    } 
 
-};
+    if (checkType(integerOne, integerTwo) == "N") return "ERROR";
+    
+    if (integerOne < 0 || integerTwo < 0) return "ERROR";
+        
+    arraySum = [];
+    finalSum = 0;
+
+    if (integerOne > integerTwo) {
+        difference = integerOne - integerTwo
+
+        for (let i = integerTwo; i <= difference + 1; i++) {
+            arraySum.push(i);
+            }
+
+    } 
+            
+    else if (integerTwo > integerOne) {
+        difference = integerTwo - integerOne;
+
+        for (let i = integerOne; i <= difference + 1; i++) {
+            arraySum.push(i);
+            } 
+    
+    }
+            
+    for (let i = 0; i < arraySum.length; i++) {
+        finalSum += arraySum[i]
+    }
+    
+    return finalSum;
+
+}
 
 // Do not edit below this line
 module.exports = sumAll;
