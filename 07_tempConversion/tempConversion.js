22d3da3732fa0c523b89f45c7ced3d72b2b73d26
kailixie <114327449+kailixie@users.noreply.github.com>
2023-01-24 18:55:30 -0500

    Complete Odin JavaScript exercise 07
    Create a function to convert Fahrenheit to Celsius and a function to convert Celsius to Fahrenheit.
    
    Use if... else with .isSafeInteger as tests fail if use Math.round() as tests against integers and numbers with one decimal place.
    
    Remove .skips to check if function passes all tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,29 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp -= 32
+  temp *= (5/9)
+
+  if (Number.isSafeInteger(temp) === true) {
+    return temp;
+  } else {
+    return Number(temp.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(temp) {
+  temp *= (9/5);
+  temp += 32;
+
+  if (Number.isSafeInteger(temp) === true) {
+    return temp;
+  } else {
+    return Number(temp.toFixed(1));
+  }
+}
+
+// .toFixed(1) returns a string with specified decimals
+// x °C ≘ (x × 9/5 + 32) °F
+//x °F ≘ (x − 32) × (5/9) °C
+
 
 // Do not edit below this line
 module.exports = {
