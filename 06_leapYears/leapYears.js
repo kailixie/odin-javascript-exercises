4528188edfcab65a9b52022bad275fde12d20fa8
kailixie <114327449+kailixie@users.noreply.github.com>
2023-01-24 18:18:07 -0500

    Complete Odin JavaScript exercise 06
    Create function to determine if year is a leap year.
    Remove all .skips to check if function passes all tests.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+// divisible by four
+// not divisible by 100
+// divisible by 400
+
+    if (year%4 === 0 && year%100 !== 0 || year%400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
